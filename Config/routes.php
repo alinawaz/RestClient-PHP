a7e7f29ee5be4397b4e9f58ee436fef9cba63103
Ali Nawaz <alinawazsolid@gmail.com>
2017-12-02 13:44:08 +0500

    Reverted to example

--- Config/routes.php
+++ Config/routes.php
@@ -6,11 +6,3 @@ use RestClient\Router as router;
 
 /* All Route Goes Here */
 router::get('', 'welcomeController@index');
-
-router::get('hello', function(){
- return "Hello World";
-});
-
-router::get('hello/{name}', function($name){
- return "Hello ".$name.", ";
-});
